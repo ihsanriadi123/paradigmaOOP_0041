7d9e6dae70900094298244874315ca5f8d6eb26e
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-20 14:37:06 +0700

    membuat kelas mahasiswa dengan float

---
+++ insolisation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+
+public:
+    mahasiswa(int pId, string pnama,float pNilai) :id(pId), nama(pnama), nilai(pNilai) {
+        //member initialization list
+          //definisi
+    }
+
+    ~mahasiswa() {
+        cout << "id = "<< id << endl;
+        cout << "nama = "<< nama << endl;
+        cout << "Nilai = "<< nilai << endl;
+    }
+};
+
+int main () {
+    mahasiswa mhd(12, "Asroni", 90.5);
+
+    return 0;
+}
