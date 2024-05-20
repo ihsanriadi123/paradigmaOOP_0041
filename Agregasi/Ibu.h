876f2c824293f961d81ce32e5afa64912432ff13
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-20 15:47:16 +0700

    membuat void ibu

--- Agregasi/Ibu.h
+++ Agregasi/Ibu.h
@@ -15,6 +15,7 @@ public:
 void ibu::tambahAnak(Anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
+
 void ibu::cetakAnak() {
     cout << "daftar anak dari ibu \"" << this->nama << "\":\n";
     for (auto& a : daftar_anak) {
